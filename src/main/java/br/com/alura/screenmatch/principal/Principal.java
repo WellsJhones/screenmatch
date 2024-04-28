2335fdc5a5bac2fc1da70fe9a478c7fbacfb9ef9
WellsJhones <wellsjhones@gmail.com>
2024-04-28 02:31:04 -0300

    implemented show episodes

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,9 +1,13 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -23,6 +27,25 @@ public class Principal {
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
 
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 1; i<=dados.totalTemporadas(); i++) {
+            json = consumoApi.obterDados ( "https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+        }
+        temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada= temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        }
+
+
 
     }
-}
